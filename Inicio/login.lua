0dccba336ef1483cc41fe8a0c110426100005e92
EzeroVivanco <zero.vivanco@gmail.com>
2014-05-21 16:56:24 -0500

    Log In
    Se agrega frame de Log In

---
+++ Inicio/login.lua
@@ -0,0 +1,85 @@
+----------------nt-------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+local widget = require( "widget" )
+local usuarioText, contrasenaText, usuarioField, contrasenaField, sesionButton, sesionButtonPress
+
+SesionButtonPress = function ( event )
+	-- body
+end
+
+function scene:create( event )
+	local sceneGroup = self.view
+
+	usuarioText = display.newText( "Nombre de Usuario o Correo Electrónico", 0, 0, native.systemFont, 15 )
+	usuarioText.x = display.contentCenterX
+	usuarioText.y = display.contentCenterY - 150
+	sceneGroup:insert( usuarioText )
+
+	contrasenaText = display.newText( "Contraseña", 0, 0, native.systemFont, 15 )
+	contrasenaText.x = display.contentCenterX
+	contrasenaText.y = display.contentCenterY - 65
+	sceneGroup:insert( contrasenaText )
+
+	usuarioField = native.newTextField( 160, display.contentCenterY - 110, 275, 50 )
+	usuarioField.font = native.newFont( native.systemFont, 15 )
+	usuarioField.placeholder = "usuario@ejemplo.com"
+	usuarioField.inputType = "email"
+	sceneGroup:insert( usuarioField )
+
+	contrasenaField = native.newTextField( 160, display.contentCenterY - 25, 275, 50 )
+	contrasenaField.font = native.newFont( native.systemFont, 15 )
+	contrasenaField.inputType = "default"
+	contrasenaField.isSecure = true
+	sceneGroup:insert( contrasenaField )
+
+	sesionButton = widget.newButton{
+		x = display.contentCenterX,
+		y = display.contentCenterY + 30,
+		label = "Iniciar Sesión",
+		font = native.systemFont,
+		fontSize = 20,
+		emboss = true,
+		onEvent = SesionBotonPress
+	}
+	sceneGroup:insert( sesionButton )
+end
+
+function scene:show( event )
+	
+	local phase = event.phase
+	
+	if "did" == phase then
+		composer.removeScene( "main" )
+	end
+	
+end
+
+function scene:hide( event )
+	
+	local phase = event.phase
+	
+	if "will" == phase then
+
+		sesionButton = nil
+	end
+	
+end
+
+function scene:destroy( event )
+	
+end
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+return scene
