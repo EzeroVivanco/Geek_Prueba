0c1634a0f9e6c5cebad140d80f6d60aa8d8c3808
EzeroVivanco <zero.vivanco@gmail.com>
2014-06-11 09:54:30 -0500

    Proyecto correcto

--- App Móvil/inicio.lua
+++ App Móvil/inicio.lua
@@ -1,33 +1,30 @@
 local composer = require( "composer" )
-local widget = require( "widget" )
-
 local scene = composer.newScene()
-
+local widget = require( "widget" )
 local _X = display.contentCenterX
 local _Y = display.contentCenterY
 local _W = display.contentWidth
 local _H = display.contentHeight
-
-local function handleButtonEvent( event )
-	if (event.phase == "ended") then
-		botonInicio = nil
-		botonRegistro = nil
-		composer.gotoScene( "formulario" )
+	local function handleButtonEvent( event )
+		if (event.phase == "ended") then
+			botonInicio = nil
+			botonRegistro = nil
+			composer.gotoScene( "formulario" )
+		end
 	end
-end
 
-local function handleButtonEvent2( event )
-	if (event.phase == "ended") then
-		botonInicio = nil
-		botonRegistro = nil
-		composer.gotoScene( "login" )
+	local function handleButtonEvent2( event )
+		if (event.phase == "ended") then
+			botonInicio = nil
+			botonRegistro = nil
+			composer.gotoScene( "principal" )
+		end
 	end
-end
 
 function scene:create( event )
 	local sceneGroup = self.view
+
 	local fondo = display.newRect( _X, _Y, _W, _H )
-	
 	fondo:setFillColor( 255,255,255 )
 	sceneGroup:insert( fondo )
 
@@ -44,6 +41,8 @@ function scene:create( event )
 	}
 	sceneGroup:insert( botonInicio)
 
+	
+
 	local botonRegistro = widget.newButton
 	{
 		x = _X,
@@ -51,6 +50,7 @@ function scene:create( event )
 		width = 150,
 		height = 50,
 		label = "Registrate",
+		--defaultFile = "design/borderbutton.png",
 		labelColor = {default={0,0,0}, over={240,248,255}},
 		fontSize = 15,
 		textOnly = true,
@@ -60,6 +60,7 @@ function scene:create( event )
 end
 
 function scene:show( event )
+	
 	local phase = event.phase
 	
 	if "did" == phase then
@@ -71,22 +72,31 @@ function scene:show( event )
 		composer.removeScene( "login" )
 	
 	end
+	
 end
 
 function scene:hide( event )
+	
 	local phase = event.phase
+	
 	if "will" == phase then
-
+	
+		print( "1: hide event, phase will" )
+	
+		-- remove touch listener for image
+		--botonRegistro:removeEventListener( "handleButtonEvent", botonRegistro )
+		--botonInicio:removeEventListener( "handleButtonEvent2", botonInicio )
+	
 	end
+	
 end
 
 function scene:destroy( event )
-	--composer.removeScene( "formulario" )
+	print( "((destroying scene 1's view))" )
 end
 
 
 scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
 scene:addEventListener( "show", scene )
