68d6366ecb3699e86c7a6422284cd78c53674571
izzy-izumi <izzyfactorial@gmail.com>
2014-06-18 14:12:07 -0500

    verificando con base de datos
    validar login

--- App Móvil/login.lua
+++ App Móvil/login.lua
@@ -22,15 +22,14 @@ local function handleButtonEvent( event )
 	if (event.phase == "ended") then
 		local path = system.pathForFile( "BD.db", system.DocumentsDirectory )
 		local db = sqlite3.open( path ) 
-
+print( db:nrows( "SELECT * FROM user WHERE email='"..usuarioField.text.."'".." and password='"..contrasenaField.text.."'" ) )
 		--for row in db:nrows("SELECT * FROM user WHERE email='"..usuarioField.text.."'".." and password='"..contrasenaField.text.."'") do
-			if db:exec( "SELECT * FROM user WHERE email='"..usuarioField.text.."'".." and password='"..contrasenaField.text.."'" ) > 0 then
+			if db:exec( "SELECT * FROM user WHERE email='"..usuarioField.text.."'".." and password='"..contrasenaField.text.."'" ) == 1 then
 			--if(row.email == usuarioField.text and row.password == contrasenaField.text) then
-		--for row in db:nrows("SELECT * FROM user WHERE email='"..usuarioField.text.."'".." and password='"..contrasenaField.text.."'") do
-			--if(row) then
-				native.setKeyboardFocus( nil )
+				print( db:nrows( "SELECT * FROM user WHERE email='"..usuarioField.text.."'".." and password='"..contrasenaField.text.."'" ) )
 				composer.gotoScene( "principal" )
 			else
+				print( db:exec( "SELECT * FROM user WHERE email='"..usuarioField.text.."'".." and password='"..contrasenaField.text.."'" ) )
 				composer.showOverlay( "popup" )
 			end
 		--end
@@ -57,32 +56,14 @@ function scene:show( event )
 	contrasenaText:setFillColor( 0,0,0 )
 	sceneGroup:insert( contrasenaText )
 
-	local function onUsername( event )
-	    if ( "began" == event.phase ) then
-	        -- This is the "keyboard appearing" event.
-	        -- In some cases you may want to adjust the interface while the keyboard is open.
-
-	    elseif ( "submitted" == event.phase ) then
-	        -- Automatically tab to password field if user clicks "Return" on virtual keyboard.
-	        native.setKeyboardFocus( contrasenaField )
-	    end
-	end
-
-	local function onPassword( event )
-	    -- Hide keyboard when the user clicks "Return" in this field
-	    if ( "submitted" == event.phase ) then
-	        native.setKeyboardFocus( nil )
-	    end
-	end
-
-	usuarioField = native.newTextField( 160, _Y - 110, 275, 50, onUsername )
+	usuarioField = native.newTextField( 160, _Y - 110, 275, 50 )
 	usuarioField.font = native.newFont( native.systemFont, 15 )
 	usuarioField.placeholder = "usuario@ejemplo.com"
 	usuarioField.inputType = "email"
 	usuarioField.align = "center"
 	sceneGroup:insert( usuarioField )
 
-	contrasenaField = native.newTextField( 160, _Y - 25, 275, 50, onPassword )
+	contrasenaField = native.newTextField( 160, _Y - 25, 275, 50 )
 	contrasenaField.font = native.newFont( native.systemFont, 15 )
 	contrasenaField.inputType = "default"
 	contrasenaField.isSecure = true
