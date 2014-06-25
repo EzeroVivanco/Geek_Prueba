5f2f2db8264afafa8771684e7ac638a6c6ca47b5
chucshary <shary-princess@hotmail.com>
2014-06-25 11:17:42 -0500

    Servidor

--- bodas/application/views/web/vwHome.php
+++ bodas/application/views/web/vwHome.php
@@ -7,9 +7,9 @@
         <!-- Ejemplo font de google -->
         <link href='http://fonts.googleapis.com/css?family=Gilda+Display' rel='stylesheet' type='text/css'>
         <!-- Estilos -->
-        <link rel="stylesheet" href="<?php echo base_url().FOUND; ?>css/foundation.css" />
-        <script type="text/javascript" src="<?php echo base_url().FOUND; ?>js/vendor/modernizr.js"></script>
-        <link rel="stylesheet" href="<?php echo base_url().CSS; ?>web/home.css" />
+        <link rel="stylesheet" href="<?php echo base_url() . FOUND; ?>css/foundation.css" />
+        <script type="text/javascript" src="<?php echo base_url() . FOUND; ?>js/vendor/modernizr.js"></script>
+        <link rel="stylesheet" href="<?php echo base_url() . CSS; ?>web/home.css" />
     </head>
     <body>
 
@@ -18,11 +18,11 @@
                 <img src="http://placehold.it/1600x600&text=Header"><br><br>
             </div>
         </div>
-        
+
 
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script type="text/javascript" src="<?php echo base_url().FOUND; ?>js/foundation.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() . FOUND; ?>js/foundation.min.js"></script>
         <!-- Funcionalidad JS de la pagina -->
-        <script type="text/javascript" src="<?php echo base_url().JS; ?>web/home.js"></script>
+        <script type="text/javascript" src="<?php echo base_url() . JS; ?>web/home.js"></script>
     </body>
 </html>
