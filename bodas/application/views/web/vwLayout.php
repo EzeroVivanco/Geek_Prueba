daa6c31dec33ae5a90d0668bf23d1073f795fff5
chucshary <shary-princess@hotmail.com>
2014-07-02 17:56:22 -0500

    Layout

--- bodas/application/views/web/vwLayout.php
+++ bodas/application/views/web/vwLayout.php
@@ -14,47 +14,50 @@
     <body> 
         <!--Header-->
         <div class="panel">
-            <div class="row text-center">
-                <div class="large-centered">
+            <div class="row">
+                <div class="small-3 small-centered columns">
                     <h1>Publicidad</h1>
                 </div>
             </div>   
         </div>
-        <div class="panel">
-            <div class="row">
-                <div class="large-3 columns">
-                    <div class="large-centered small-block-grid-1">
-                        <h6>Siguenos
-                            <a href ='https://www.facebook.com/pages/Geek-Bucket-Fabrica-de-software/165847196760102?fref=ts' ><img src="<?php echo base_url() . IMG; ?>web/facebook.png" width="20px" ></a>
-                            <a href = "https://twitter.com/" ><img src="<?php echo base_url() . IMG; ?>web/twiter.png" width="35px"></a>
-                            <a href = "#" ><img src="<?php echo base_url() . IMG; ?>web/p.png" width="35px"></a>
-                            <a href = "http://instagram.com/" ><img src="<?php echo base_url() . IMG; ?>web/instagram.png" width="30px"></a>
-                        </h6>
-                    </div>
-                </div>
 
-                <div class="large-5 columns">
-                    <div class="large-centered">
-                        <img src="<?php echo base_url() . IMG; ?>web/honeymooners.png" width="400px">
-                    </div> 
-                </div>
 
-                <div class="large-3 columns">
-                    <div class="large-centered">
-                        <div class="row">
-                            <div class="small-10 columns">
-                                <input class="small-2 columns text-center" type="text" placeholder="BUSCAR"/>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="small-10 columns">
-                                <a onclick="location.href = '<?php echo base_url('web/Registro') ?>'" id="boton" class="button right" >Registrate</a>
-                            </div>
+        <div class="contain-to-grid fond">
+            <div class="row small-centered columns">
+                <div class="small-2 large-4 columns" id="borde">
+                    <img src="<?php echo base_url() . IMG; ?>web/siguenos.png" width="150px" >
+                    <a href ='https://www.facebook.com/pages/Geek-Bucket-Fabrica-de-software/165847196760102?fref=ts' ><img src="<?php echo base_url() . IMG; ?>web/facebook.png" width="20px"></a>
+                    <a href = "https://twitter.com/" ><img src="<?php echo base_url() . IMG; ?>web/twiter.png" width="35px"></a>
+                    <a href = "#" ><img src="<?php echo base_url() . IMG; ?>web/p.png" width="35px"></a>
+                    <a href = "http://instagram.com/" ><img src="<?php echo base_url() . IMG; ?>web/instagram.png" width="34px"></a>
+                </div>
+                <div class="small-4 large-4 columns">
+                    <img src="<?php echo base_url() . IMG; ?>web/honeymooners.png">
+                </div>
+                <div class="small-6 large-4 columns">
+                    <div class="row ">
+                        <div class="small-10 small-centered columns">
+                            <input class="text-center" type="text" id="borde" placeholder="BUSCADOR"/>
                         </div>
                     </div>
-
                 </div>
             </div>
+        </div>
+
+        <div class=" panel">
+
+
+        </div>
+
+        <div class="contain-to-grid fond">
+            <div class="row">
+                <div class="small-2 large-4 columns"></div>
+                <div class="small-4 large-4 columns"></div>
+                <div class="small-6 large-4 columns text-center">
+                    <a onclick="location.href = '<?php echo base_url('web/Registro') ?>'"><img src="<?php echo base_url() . IMG; ?>web/registrate_1.png" width="220px"></a>
+                </div>
+            </div>
+
         </div>
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url() . FOUND; ?>js/foundation.min.js"></script>
