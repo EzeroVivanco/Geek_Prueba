315f8f0402b79a20501f6c5e42d13a37146c4be0
alejandrocan <darknesstp@gmail.com>
2014-07-30 12:57:16 -0500

    Correcion

--- bodas/application/views/admin/vwHeader.php
+++ bodas/application/views/admin/vwHeader.php
@@ -4,26 +4,12 @@
             <li><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/corazon.png" class="imagen1"></a></li>
             
         </ul>
-        <!-- <section class="top-bar-section">
-            <ul class="left">
-                <li class="texto"><a onclick="location.href = '<?php echo base_url('admin/definir/index') ?>'"><img src="<?php echo base_url() . IMG; ?>web/dash/define.png" class="imagen" >Define</a></li>
-                <li class="texto"><a onclick="location.href = '<?php echo base_url('admin/organiza/index') ?>'"><img src="<?php echo base_url() . IMG; ?>web/dash/organiza.png" class="imagen">Organiza</a></li>
-                <li class="texto"><a onclick="location.href = '<?php echo base_url('admin/actividades/index') ?>'"><img src="<?php echo base_url() . IMG; ?>web/dash/actividades.png" class="imagen">Actividades</a></li>
-                <li class="texto"><a onclick="location.href = '<?php echo base_url('admin/invitados/index') ?>'"><img src="<?php echo base_url() . IMG; ?>web/dash/invitation.png" class="imagen">Invitados</a></li>
-                <li class="texto"><a onclick="location.href = '<?php echo base_url('admin/regalos/index') ?>'"><img src="<?php echo base_url() . IMG; ?>web/dash/mesa_regalos.png" class="imagen">Mesa de regalos</a></li>
-                <li class="texto"><a onclick="location.href = '<?php echo base_url('admin/mensajes/index') ?>'"><img src="<?php echo base_url() . IMG; ?>web/dash/message.png" class="imagen">Mensajes</a></li>
-            </ul>
-            <ul class="right">
-                <li><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/config.png" class="imagen">Ajustes</a></li>
-                <li><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/salir.png" class="imagen">Salir</a></li>
-            </ul>
-        </section> -->
         <section>
             <ul class="list-menu-left">
                 <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/define.png" class="imagen"><br />Define</a></li>
                 <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/organiza.png" class="imagen"><br />Organiza</a></li>
-                <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/actividades.png"class="imagen"><br/>Actividades</a></li>
-                <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/invitation.png" class="imagen"><br/></b>invitados</a></li>
+                <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/actividades.png" class="imagen"><br/>Actividades</a></li>
+                <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/invitation.png" class="imagen"><br/>invitados</a></li>
                 <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/mesa_regalos.png" class="imagen"><br/>Mesa de regalos</a></li>
                 <li class="item"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/message.png" class="imagen"><br />Mensajes</a></li>
                  
@@ -31,7 +17,7 @@
             
             <ul class="list-menu-right">
                 <li class="item-right"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/config.png" class="imagen-right"><br />Ajustes</a></li>
-                <li class="item-right"><a onclick="logout()"><img src="<?php echo base_url() . IMG; ?>web/dash/salir.png" class="imagen-right"><br />Salir</a></li>
+                <li class="item-right"><a href="#"><img src="<?php echo base_url() . IMG; ?>web/dash/salir.png" class="imagen-right"><br />Salir</a></li>
                 
             </ul>
             
@@ -51,7 +37,6 @@
         </center>
     </div>
 </div>
-<body>
     <div class="row">
         
     </div>
--- bodas/application/views/admin/vwPortal.php
+++ bodas/application/views/admin/vwPortal.php
@@ -20,6 +20,8 @@
             <label for="tab-1">Ubicacion</label>
             <input id="tab-2" type="radio" name="tab-group" />
             <label for="tab-2">Informacion</label>
+            <input id="tab-3" type="radio" name="tab-group" />
+            <label for="tab-3">Proveedores</label>
             <div id="content">
                 <div id="content-1">
                     <p class="left">Lorem ipsum dolor sit amet, consectetur adipisicing elit, Voluptamum sit reprehenderit iusto harum  minima. Assemenda, accusamus, perspiciatis inventore tempora quipariatur quismam? Deleniti, placeat ea nostrum officiil obcaecati temporibus quop. Ullam, in adipisci autem at fugit ab tempore enim raione nesciunt alias corporis vitae quo quod nostrim itaque vero iure?</p>
@@ -32,12 +34,15 @@
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, Voluptamum sit reprehenderit iusto harum  minima. Assemenda, accusamus, perspiciatis inventore tempora quipariatur quismam? Deleniti, placeat ea nostrum officiil obcaecati temporibus quop. Ullam, in adipisci autem at fugit ab tempore enim raione nesciunt alias corporis vitae quo quod nostrim itaque vero iure?
                     </p>
                 </div>
+                <div id="content-3">
+                    <p> Contenido de los proveedores
+                    </p>
+                </div>
             </div>
         </div>
         <!--Fin-->
         <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url() . FOUND; ?>js/foundation.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url() . JS; ?>web/home.js"></script>
-        <script type="text/javascript" src="<?php echo base_url() . JS; ?>web/logout.js"></script>
     </body>
 </html>
